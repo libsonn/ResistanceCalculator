387388c00608b2e414d790c8e10b10968bb4aa89
Wojciech Liberacki <w.liberacki@gmail.com>
2021-05-21 12:06:49 +0200

    BLoC refactorization

--- lib/logic/bloc/resistance_calculator_bloc.dart
+++ lib/logic/bloc/resistance_calculator_bloc.dart
@@ -13,46 +13,64 @@ part 'resistance_calculator_state.dart';
 
 class ResistanceCalculatorBloc
     extends Bloc<ResistanceCalculatorEvent, ResistanceCalculatorState> {
-  CalculationLogic _calculationLogic = CalculationLogic();
-
-  ResistanceCalculatorBloc() : super(ResistanceCalculatorInitial());
+  final CalculationLogic calculationLogic;
+  ResistanceCalculatorBloc({@required this.calculationLogic})
+      : super(ResistanceCalculatorInitial());
 
   @override
   Stream<ResistanceCalculatorState> mapEventToState(
     ResistanceCalculatorEvent event,
   ) async* {
     if (event is AddLine) {
-      ResistorModel resistorModel;
-      if (state.resistorModel.numberOfLines < 5) {
-        resistorModel = state.resistorModel
-            .copyWith(numberOfLines: state.resistorModel.numberOfLines + 1);
-        resistorModel =
-            _calculationLogic.calculateResistance(resistor: resistorModel);
-        yield ChangedValuesState(resistorModel: resistorModel);
-      }
+      yield* mapAddLine(event);
     }
     if (event is RemoveLine) {
-      ResistorModel resistorModel;
-      if (state.resistorModel.numberOfLines > 3) {
-        resistorModel = state.resistorModel
-            .copyWith(numberOfLines: state.resistorModel.numberOfLines - 1);
-
-        resistorModel =
-            _calculationLogic.calculateResistance(resistor: resistorModel);
-
-        yield ChangedValuesState(resistorModel: resistorModel);
-      }
+      yield* mapRemoveLine(event);
     }
     if (event is SelectLine) {
-      ResistorModel resistorModel;
-      List<Line> listOfSelectedLines = state.resistorModel.listOfSelectedLines;
-      listOfSelectedLines[event.numberOfLine] = event.selectedLine;
-      resistorModel = state.resistorModel
-          .copyWith(listOfSelectedLines: listOfSelectedLines);
+      yield* mapSelectLine(event);
+    }
+  }
 
+  Stream<ResistanceCalculatorState> mapAddLine(
+    AddLine event,
+  ) async* {
+    ResistorModel resistorModel;
+    if (state.resistorModel.numberOfLines < 5) {
+      resistorModel = state.resistorModel
+          .copyWith(numberOfLines: state.resistorModel.numberOfLines + 1);
       resistorModel =
-          _calculationLogic.calculateResistance(resistor: resistorModel);
+          calculationLogic.calculateResistance(resistor: resistorModel);
       yield ChangedValuesState(resistorModel: resistorModel);
     }
+  }
+
+  Stream<ResistanceCalculatorState> mapRemoveLine(
+    RemoveLine event,
+  ) async* {
+    ResistorModel resistorModel;
+    if (state.resistorModel.numberOfLines > 3) {
+      resistorModel = state.resistorModel
+          .copyWith(numberOfLines: state.resistorModel.numberOfLines - 1);
+
+      resistorModel =
+          calculationLogic.calculateResistance(resistor: resistorModel);
+
+      yield ChangedValuesState(resistorModel: resistorModel);
+    }
+  }
+
+  Stream<ResistanceCalculatorState> mapSelectLine(
+    SelectLine event,
+  ) async* {
+    ResistorModel resistorModel;
+    List<Line> listOfSelectedLines = state.resistorModel.listOfSelectedLines;
+    listOfSelectedLines[event.numberOfLine] = event.selectedLine;
+    resistorModel =
+        state.resistorModel.copyWith(listOfSelectedLines: listOfSelectedLines);
+
+    resistorModel =
+        calculationLogic.calculateResistance(resistor: resistorModel);
+    yield ChangedValuesState(resistorModel: resistorModel);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:resistance_calculator/constants.dart';
 import 'package:resistance_calculator/logic/bloc/resistance_calculator_bloc.dart';
+import 'package:resistance_calculator/logic/calculation_logic.dart';
 import 'package:resistance_calculator/presentation/screens/home_page.dart';
 
 void main() {
@@ -12,7 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => ResistanceCalculatorBloc(),
+      create: (context) => ResistanceCalculatorBloc(
+        calculationLogic: new CalculationLogic(),
+      ),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Resistance Calculator',
